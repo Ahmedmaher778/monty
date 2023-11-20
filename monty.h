fc8c505c324e496fd1744bbc7416d23b9a756d6f
Ahmedmaher778 <139475659+Ahmedmaher778@users.noreply.github.com>
2023-11-20 02:07:46 +0200

    Update monty.h

--- monty.h
+++ monty.h
@@ -11,53 +11,51 @@
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
+ * @num: integer
+ * @prev: points to previous element of the stack (or queue)
+ * @next: points to next element of the stack (or queue)
  * Description: doubly linked list node structure
  * for stack, queues, LIFO, FIFO
- */
+*/
+
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int num;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
  * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
+ * @opcde: the opcode
+ * @fun: function to handle opcode
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
- */
+*/
+
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcde;
+	void (*fun)(stack_t **stack, unsigned int lin_nmber);
 } instruction_t;
 
 extern stack_t *head;
 typedef void (*op_func)(stack_t **, unsigned int);
 
 /*file operations*/
-void open_file(char *file_name);
-int parse_line(char *buffer, int line_number, int format);
+void open_file(char *fle_name);
+int parse_line(char *bufer, int lin_nmber, int format);
 void read_file(FILE *);
 int len_chars(FILE *);
 void find_func(char *, char *, int, int);
 
 /*Stack operations*/
-stack_t *create_node(int n);
+stack_t *create_node(int num);
 void free_nodes(void);
 void print_stack(stack_t **, unsigned int);
 void add_to_stack(stack_t **, unsigned int);
 void add_to_queue(stack_t **, unsigned int);
-
 void call_fun(op_func, char *, char *, int, int);
-
 void print_top(stack_t **, unsigned int);
 void pop_top(stack_t **, unsigned int);
 void nop(stack_t **, unsigned int);
@@ -76,9 +74,9 @@ void print_str(stack_t **, unsigned int);
 void rotl(stack_t **, unsigned int);
 
 /*Error hanlding*/
-void err(int error_code, ...);
-void more_err(int error_code, ...);
-void string_err(int error_code, ...);
+void error(int eror_cde, ...);
+void more_error(int eror_cde, ...);
+void string_error(int eror_cde, ...);
 void rotr(stack_t **, unsigned int);
 
 #endif
